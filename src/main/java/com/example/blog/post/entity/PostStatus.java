7709ee742676d18f65b66bdca287e57ddc2e7d95
Maeng <aodaod128@naver.com>
2025-12-23 03:46:27 +0900

    fix: fix entity

--- src/main/java/com/example/blog/post/entity/PostStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.blog.post.entity;
-
-public enum PostStatus {
-    DRAFT , PUBLISHED
-    // 미완   완
-}
--- src/main/java/com/example/blog/user/entity/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.example.blog.user.entity;
-
-public enum UserRole {
-    USER, ADMIN
-}
